56d189a86896c4dce82a9da2c2fd664d3fcabf7a
wickedstar <60044233+wickedstar115@users.noreply.github.com>
2023-09-11 20:25:24 +0000

    Update Zoom Browser Autojoin.user.js

--- Scripts/Zoom Browser Autojoin.user.js
+++ Scripts/Zoom Browser Autojoin.user.js
@@ -1,13 +1,30 @@
 // ==UserScript==
-// @name         Zoom Webclient Redirect (RVA)
-// @namespace    https://www.RVAFixes.tk/
-// @version      0.1
-// @description  Redirects you to the Zoom webclient automaticly!
-// @author       Binkers Gaming / Chris
-// @match        *://ruralvirtual.zoom.us/j/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
+// @name           Zoom redirector
+// @namespace      https://openuserjs.org/users/clemente
+// @match          https://*.zoom.us/j/*
+// @match          https://*.zoom.us/s/*
+// @version        1.2
+// @author         clemente
+// @license        MIT
+// @description    Transparently redirects any meeting links to use Zoom's browser based web client
+// @icon           https://i.imgur.com/y8BXA8l.png
+// @inject-into    content
+// @run-at         document-start
+// @updateURL      https://openuserjs.org/meta/clemente/Zoom_redirector.meta.js
+// @downloadURL    https://openuserjs.org/install/clemente/Zoom_redirector.user.js
+// @supportURL     https://openuserjs.org/scripts/clemente/Zoom_redirector/issues
+// @homepageURL    https://openuserjs.org/scripts/clemente/Zoom_redirector
+// @noframes
 // ==/UserScript==
 
-window.location = window.location.toString().replace('ruralvirtual.zoom.us/j/', 'ruralvirtual.zoom.us/wc/join/');
-console.log("redirect")
+const url = new URL(document.URL);
+const match = /^\/[js]\/(\d+)\/?$/.exec(url.pathname);
+if (match === undefined || match === null || match[1] === undefined) {
+  return;
+}
+
+const urlEnding = match[0][1];
+const meetingId = match[1];
+const mapping = {'j': '/join', 's': '/start'};
+
+document.location.pathname = '/wc/' + encodeURIComponent(meetingId) + mapping[urlEnding];
